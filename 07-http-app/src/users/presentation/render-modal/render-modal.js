286af046e86d4b9d16133a7eecc1df1e62e0c35d
randymz10 <randymz10@gmail.com>
2023-02-07 23:51:40 -0300

    Add new features

--- 07-http-app/src/users/presentation/render-modal/render-modal.js
+++ 07-http-app/src/users/presentation/render-modal/render-modal.js
@@ -1,17 +1,40 @@
 import modalHtml from './render-modal.html?raw';
+import { User } from "../../models/user";
+import { getUserById } from '../../use-cases/get-user-by-id';
 import './render-modal.css';
 
 let modal, form;
+let loadedUser = {};
 
-//TODO: cargar usuario por id
-export const showModal = () => {
+/**
+ * 
+ * @param {String|Number} id 
+ */
+export const showModal = async( id ) => {
     modal?.classList.remove('hide-modal');
+    let loadedUser = {};
+    
+    if( !id ) return;
+    const user = await getUserById( id );
+    setFormValues( user );
 }
 
 export const hideModal = () => {
     modal?.classList.add('hide-modal');
 
     form?.reset();
+}
+
+/**
+ * 
+ * @param {User} user 
+ */
+const setFormValues = ( user ) => {
+    form.querySelector('[name="firstName"]').value = user.firstName;
+    form.querySelector('[name="lastName"]').value = user.lastName;
+    form.querySelector('[name="balance"]').value = user.balance;
+    form.querySelector('[name="isActive"]').checked = user.isActive;
+    loadedUser = user;
 }
 
 /**
@@ -38,7 +61,7 @@ export const renderModal = ( element, callback ) => {
         event.preventDefault();
 
         const formData = new FormData( form );
-        const userLike = {};
+        const userLike = { ...loadedUser };
         for (const [key, value] of formData) {
             if( key === 'balance' ) {
                 userLike[key] = +value;
@@ -53,6 +76,7 @@ export const renderModal = ( element, callback ) => {
             userLike[key] = value;
         }
         await callback( userLike );
+
         hideModal();
     });
 
--- 07-http-app/src/users/presentation/render-table/render-table.js
+++ 07-http-app/src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from "../../store/users-store";
+import { showModal } from "../render-modal/render-modal";
 import "./render-table.css";
 
 let table;
@@ -22,6 +23,14 @@ const createTable = () => {
   return table;
 };
 
+const tableSelectListener = ( event ) => {
+  const element = event.target.closest('.select-user');
+  if( !element ) return;
+
+  const id = element.getAttribute('data-id');
+  showModal(id);
+}
+
 /**
  *
  * @param {HTMLDivElement} element
@@ -35,6 +44,7 @@ export const renderTable = (element) => {
     element.append(table);
 
     //TODO: Listeners a la tabla
+    table.addEventListener('click', tableSelectListener );
   }
 
   let tableHTML = "";
@@ -47,9 +57,9 @@ export const renderTable = (element) => {
         <td>${user.lastName}</th>
         <td>${user.isActive}</th>
         <td>
-            <a href="#" data-id="${user.id}">Select</a>
+            <a href="#" class="select-user" data-id="${user.id}">Select</a>
             | 
-            <a href="#" data-id="${user.id}">Delete</a>
+            <a href="#" class="delete-user" data-id="${user.id}">Delete</a>
             
         </th>
         </tr>
